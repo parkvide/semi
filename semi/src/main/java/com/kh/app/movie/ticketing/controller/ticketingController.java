f8fc44e60bca37f19bcd3566f3ee5e267c1e171c
Park Jindong <dmosier@naver.com>
2024-05-23 09:00:03 +0900

    0523

--- semi/src/main/java/com/kh/app/movie/ticketing/controller/ticketingController.java
+++ semi/src/main/java/com/kh/app/movie/ticketing/controller/ticketingController.java
@@ -1,6 +1,7 @@
 package com.kh.app.movie.ticketing.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.app.movie.service.MovieTicketingService;
+import com.kh.app.movie.vo.MovieVo;
+
 @WebServlet("/movie/ticketing")
 public class ticketingController extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	
+	MovieTicketingService mts = new MovieTicketingService();
+	
+	try {
+		List<MovieVo> voList = mts.selectMovieList();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
 	
 	req.getRequestDispatcher("/WEB-INF/views/movie/ticketing.jsp").forward(req, resp);
 	
